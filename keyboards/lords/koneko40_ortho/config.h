dc6f721a06150b82e87f72501694fc7808e545c5
Kelly Helmut Lord <helmut@helmutlord.com>
2023-08-19 17:40:21 -0400

    added koneko40s

---
+++ keyboards/lords/koneko40_ortho/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+
+#define RGBLIGHT_LIMIT_VAL 200
+
+#define RGBLED_NUM 12
+#define WS2812_DI_PIN B9
+#define NOP_FUDGE 0.4
+
---
+++ keyboards/lords/koneko40_stagger/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x28, 0xFC, 0x7F, 0x68, 0xB4, 0xCA, 0x05, 0x10}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
